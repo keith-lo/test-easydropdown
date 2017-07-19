acba0a5f9f7e4f3409fa7e2552447d45862220b8
Keith M.F. Lo <p4837@cmhk.local>
2017-07-19 11:41:49 +0800

    Disable select sample

--- protected/views/form/index.php
+++ protected/views/form/index.php
@@ -3,17 +3,27 @@
 
 <?=
 	CHtml::dropDownList('field-name', 'option2', 
-		array('option1' => 'Option 1', 'option2' => 'Option 2', 'option3' => 'Option 3', 'option4' => 'Option 4'), 
+		array('option1' => 'Option 1 (No stock)', 'option2' => 'Option 2', 'option3' => 'Option 3', 'option4' => 'Option 4'),
 		array(
 			'class' => 'mydropdown',
 			'options' => array(
-				'option1' => array('class' => 'text-danger'),
+				'option1' => array('class' => 'text-danger', 'disabled'=>'disabled'),
 				'option2' => array('class' => 'bg-warning'),
 			)
 		)
-	)
+	);
 ?>
 
+<!--
+<select class="mydropdown" name="field-name" id="field-name">
+	<option value="option1" class="text-danger" disabled="disabled">Option 1 (No stock)</option>
+	<option value="option2" selected="selected" class="bg-warning">Option 2</option>
+	<option value="option3">Option 3</option>
+	<option value="option4">Option 4</option>
+</select>
+-->
+
 <style type="text/css">
+	.dropdown.disabled{ background: #808080; }
 	.dropdown .text-danger{ color: #a94442 !important; }
 </style>
--- themes/bootstrap/js/form/index.js
+++ themes/bootstrap/js/form/index.js
@@ -1,8 +1,46 @@
 $(function(){
-    $('select.mydropdown').easyDropDown({
+
+    $('#field-name').easyDropDown({
+        //disabled: true, //Turn this on to disable easyDropDown
         onChange: function(obj){
             console.log('Selected', obj);
-        }
+        },
+        select: function(index){
+			var self = this;
+
+			if(typeof index === 'string'){
+				index = self.$select.find('option[value='+index+']').index() - 1;
+			};
+
+			var	option = self.options[index],
+                selectIndex = self.hasLabel ? index + 1 : index;
+
+            //Custom code : start
+            if( self.$select.find('option').eq(selectIndex).is(':disabled') ){ return false; }
+            //Custom code : end
+
+			self.$items.removeClass('active').eq(index).addClass('active');
+			self.$active.text(option.title);
+			self.$select
+				.find('option')
+				.removeAttr('selected')
+				.eq(selectIndex)
+				.prop('selected',true)
+				.parent()
+				.trigger('change');
+
+			self.selected = {
+				index: index,
+				title: option.title
+			};
+			self.focusIndex = i;
+			if(typeof self.onChange === 'function'){
+				self.onChange.call(self.$select[0],{
+					title: option.title,
+					value: option.value
+				});
+			};
+		}
     });
 
     $('select.mydropdown option').each(function(){
@@ -22,4 +60,5 @@ $(function(){
             }
         }
     });
+
 });
